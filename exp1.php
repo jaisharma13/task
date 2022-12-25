a5ee6fa20e70e53db1b8d3c9804269d263865254
jai <you@example.com>
2022-12-25 19:21:42 +0530

    exp1

---
+++ exp1.php
@@ -0,0 +1,26 @@
+
+<?php
+
+
+$arr = [1, 2, 3, 4, 5];
+$len = count($arr);
+
+
+$res =array();
+
+
+for ($i=0; $i<=$len ; $i++) 
+{ 
+   $arr1= array_filter($arr, function($v, $k)
+    {
+        global $i;
+        return $k != $i;
+
+    },
+    ARRAY_FILTER_USE_BOTH);
+    array_push($res, array_product($arr1)); 
+
+}
+print_r($res);
+
+?>
